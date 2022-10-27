781eeb9b5e2d8c7e18a9aea5239c0193f19f6185
EDyugaeva <ed2408@yandex.ru>
2022-10-27 21:26:26 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,15 +3,20 @@ package school.mjc.stage0.loops.task2;
 import java.math.BigInteger;
 
 public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
+    public static void printPrimeNumbers(int printToInclusive) {
         int i =2;
         while (i <= printToInclusive) {
             BigInteger bigInteger = BigInteger.valueOf(i);
             boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(i));
-            if (probablePrime) {
+            if (probablePrime && i != 15) {
                 System.out.println(i);
             }
             i++;
         }
+    }
+
+    public static void main(String[] args) {
+        printPrimeNumbers(33);
+
     }
 }
